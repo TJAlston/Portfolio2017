35dde71b833f4cb63d06c3d8eabbd9869bf47561
TJAlston <tjalston82@gmail.com>
2016-11-03 21:55:00 -0400

    finished linking

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import About from './About'
 import Skills from './Skills'
 import Portfolio from './Portfolio'
 import Contact from './Contact'
+import { HashLink } from 'react-router-hash-scroll'
 
 class App extends Component {
 
@@ -18,8 +19,8 @@ class App extends Component {
           <h2>Front End Web Developer</h2>
           <p>Creating beautiful dynamic webpages from a blank page is what I love to do! </p>
           <div className='buttons'>
-            <button>Portfolio</button>
-            <button>Contact</button>
+            <button><HashLink to='/#portfolio'>Portfolio</HashLink></button>
+            <button><HashLink to='/#contact'>Contact</HashLink></button>
           </div>
         </div>
         <div className='links'>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,20 +5,10 @@ import { HashLink } from 'react-router-hash-scroll'
 
 class Header extends Component {
   render () {
-      //   componentDidUpdate() {
-      //   let hash = this.props.location.hash.replace('#', '')
-      //   if (hash) {
-      //       let node = ReactDOM.findDOMNode(this.refs[hash])
-      //       if (node) {
-      //           node.scrollIntoView()
-      //       }
-      //   }
-      // }
-
     return <div className='header'>
     <nav>
       <ul>
-       <li className='name'><HashLink to='/#'>Tameka J. Alston</HashLink></li>
+       <li className='name'><HashLink to='/#home'>Tameka J. Alston</HashLink></li>
         <li><HashLink to='/#about'>ABOUT</HashLink></li>
         <li><HashLink to='/#skills'>SKILLS</HashLink></li>
         <li><HashLink to='/#portfolio'>PORTFOLIO</HashLink></li>
