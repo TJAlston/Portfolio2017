0e61b8ee33d9533506fccc3ce197743936ba5c93
TJAlston <tjalston82@gmail.com>
2016-12-13 18:15:59 -0500

    made transition

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -23,8 +23,9 @@ class Portfolio extends Component {
       </div>
 
       <div id='graphic'>
+
       <h5> Graphic Design </h5>
-      <div className='row1'>
+      <div className='row1' id='graphicRow'>
 
         <div className='thumb'>
           <a href='#lightbox1'>
